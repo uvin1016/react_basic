f18d82faa5b0b7df6c8ee007768fa4a251332e48
uvin1016 <uvin1016@naver.com>
2021-12-15 22:48:31 +0900

    step6 public 이미지 불러오기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './css/style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import image1 from './img/member1.jpg';
-import image2 from './img/member2.jpg';
+
 
 function App() {
-  let divStyle = {width: 100, height: 100, backgroundImage: `url(${image2})`, backgroundSize: "cover"};
+  let divStyle = {width: 100, height: 100, backgroundImage: `url(${process.env.PUBLIC_URL+'img/member2.jpg'})`, backgroundSize: "cover"};
   return (
     <figure>
       <Header />
-          <img src={image1} />
-          <img src={image2} />
+          <img src={process.env.PUBLIC_URL+'img/member1.jpg'} />
+          <img src={process.env.PUBLIC_URL+'img/member2.jpg'} />
 
           <div style={divStyle}>
 
