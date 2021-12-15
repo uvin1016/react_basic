93955428dca1b76517551a44c3a98bb9497bb469
uvin1016 <uvin1016@naver.com>
2021-12-15 22:44:15 +0900

    step5 이미지 불러오기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './css/style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import image1 from './img/member1.jpg';
+import image2 from './img/member2.jpg';
 
 function App() {
+  let divStyle = {width: 100, height: 100, backgroundImage: `url(${image2})`, backgroundSize: "cover"};
   return (
     <figure>
       <Header />
+          <img src={image1} />
+          <img src={image2} />
 
+          <div style={divStyle}>
+
+          </div>
       <Footer />
     </figure>
   );
