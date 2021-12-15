c135124c7914460ae475744dfb2faf7c9b8468c7
uvin1016 <uvin1016@naver.com>
2021-12-15 21:35:24 +0900

    step2 sass 환경설정 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import './App.css';
+import './css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <figure>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </figure>
   );
 }
 
