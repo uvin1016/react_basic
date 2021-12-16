ef969e7ff05d75f5c8c467ef11e51b04a35da7cb
uvin1016 <uvin1016@naver.com>
2021-12-16 20:17:16 +0900

    step9 useState로 상태관리

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import './css/style.css';
 import Header from './components/Header';
 import Panels  from './components/Panels';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 
 function App() {
   const arr = ["member1","member2","member3","member4","member5","member6","member7","member8"];
   const deg = 360/arr.length;
+  let [txt, setTxt] = useState(arr);
 
   return (
     <figure>
+      <button onClick={()=>{
+        let txt2 = [...txt];
+        txt2[0] = 'member7';
+        setTxt(txt2);
+      }}>버튼</button>
       <Header />
         <section>
           {
-            arr.map((data,index)=>{
+            txt.map((data,index)=>{
               return <Panels key={index} num={index} txt={data} deg={deg} />
             })
           }
