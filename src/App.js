d06a79540578264231ff2de3dfcab7a5301114b2
uvin1016 <uvin1016@naver.com>
2021-12-16 19:47:13 +0900

    step8 map으로 가상 DOM 반복처리

--- src/App.js
+++ src/App.js
@@ -4,24 +4,33 @@ import Footer from './components/Footer';
 
 
 function App() {
-  let picImg = {backgroundImage: `url(${process.env.PUBLIC_URL}/img/member1.jpg)`}
+  const arr = ["member1","member2","member3","member4","member5","member6","member7","member8"];
+  const deg = 360/arr.length;
 
   return (
     <figure>
       <Header />
         <section>
-          <article>
-            <div className="inner">
-              <div className="pic" style={picImg}>
-                <div className="dot"></div>
-              </div>
+          {
+            arr.map((data,index)=>{
+              let rotatePanel = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}
+              let picImg = {backgroundImage: `url(${process.env.PUBLIC_URL}/img/${data}.jpg)`}
 
-              <div className="txt">
-                <h2>Title</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </div>
-            </div>
-          </article>
+              return(
+                <article key={index} style={rotatePanel}>
+                  <div className="inner">
+                    <div className="pic" style={picImg}>
+                      <div className="dot"></div>
+                    </div>
+                    <div className="txt">
+                      <h2>{data}</h2>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                  </div>
+                </article>
+              )
+            })
+          }
         </section>
       <Footer />
     </figure>
